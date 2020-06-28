38f9dd107644939309a02d7dfbbfa9dc334ea804
Mauricio Rojas <mrojasb2000@gmail.com>
2020-06-28 16:07:02 -0400

    Add http server expressive.

--- httpserver.ts
+++ httpserver.ts
@@ -6,6 +6,11 @@ import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-expr
     app.get('/api/todos', async (req, res) => {
         await res.json([{todoName: 'Ponerme la mascarilla'}]);
     });
+
+    app.get('/api/todos/{id}', async (req, res) => {
+        await res.json([{todoID: req.params.id}]);
+    });
+
     const server = await app.listen(port);
     console.log('app ready to listen connections on port ' + server.port);
 })();
